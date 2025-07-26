c082907393fbd9de05fa0d6d6fdcb2b262e8d293
ICP.Ninja <noreply@icp.ninja>
2025-07-26 19:53:11 +0300

    ICP.Ninja export

---
+++ src/declarations/user_canister/user_canister.did.d.ts
@@ -0,0 +1,37 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Campaign {
+  'id' : CampaignId,
+  'owner' : UserId,
+  'name' : string,
+  'createdAt' : bigint,
+  'acceptedTokens' : Array<string>,
+  'description' : string,
+}
+export type CampaignId = string;
+export interface User {
+  'id' : UserId,
+  'name' : string,
+  'createdAt' : bigint,
+  'email' : [] | [string],
+}
+export interface UserCanister {
+  'clearUsers' : ActorMethod<[], undefined>,
+  'createCampaign' : ActorMethod<[string, string, Array<string>], string>,
+  'createUser' : ActorMethod<[string, [] | [string]], boolean>,
+  'debugCompare' : ActorMethod<[UserId], Array<[string, Principal, boolean]>>,
+  'debugPrincipal' : ActorMethod<[UserId], string>,
+  'getAllCampaigns' : ActorMethod<[], Array<Campaign>>,
+  'getAllUsers' : ActorMethod<[], Array<User>>,
+  'getCampaign' : ActorMethod<[string], [] | [Campaign]>,
+  'getPrincipal' : ActorMethod<[], Principal>,
+  'getUserCampaigns' : ActorMethod<[UserId], Array<Campaign>>,
+  'userExists' : ActorMethod<[], boolean>,
+  'whoami' : ActorMethod<[], Principal>,
+}
+export type UserId = Principal;
+export interface _SERVICE extends UserCanister {}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
