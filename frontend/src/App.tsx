eb6c88102c57fab4c396eb78b7a58d08f9e451f6
ICP.Ninja <noreply@icp.ninja>
2025-07-26 19:52:55 +0300

    ICP.Ninja export

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainApp from "./MainApp";
+import CampaignPage from "./CampaignPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainApp />} />
+        <Route path="/donate/:id" element={<CampaignPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
